cd8ce4b6964577f3b16b2842d1f8d3e0d5cb7ab8
Dmitry Morozov <libertariansessions@gmail.com>
2022-04-16 12:54:09 +0500

    parser

--- src/Domain/Levels.cs
+++ src/Domain/Levels.cs
@@ -1,4 +1,6 @@
-﻿namespace thegame.Domain
+﻿using System.Linq;
+
+namespace thegame.Domain
 {
     public static class Levels
     {
@@ -11,6 +13,34 @@
                 {CellType.Box, CellType.Box, CellType.Box},
                 {CellType.Box, CellType.Box, CellType.Box},
             };
+        }
+
+        public static CellType[,] ParseFromString(string[] lines)
+        {
+            var cells = new CellType[lines[0].Length, lines.Length];
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    cells[x, y] = SwitchFromChar(lines[y][x]);
+                }
+            }
+
+            return cells;
+        }
+
+        public static CellType SwitchFromChar(char c)
+        {
+            switch (c)
+            {
+                case 'W': return CellType.Wall;
+                case 'E': return CellType.Empty;
+                case 'P': return CellType.Player;
+                case 'T': return CellType.Target;
+                case 'N': return CellType.BoxOnTarget;
+                case 'B': return CellType.Box;
+                default: return CellType.Empty;
+            }
         }
     }
 }
