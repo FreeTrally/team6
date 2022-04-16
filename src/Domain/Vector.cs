10a69ca74312e1de873bff7ab2277a3326630184
Dmitry Morozov <libertariansessions@gmail.com>
2022-04-16 12:43:35 +0500

    vector

--- src/Domain/Vector.cs
+++ src/Domain/Vector.cs
@@ -1,4 +1,36 @@
 ﻿namespace thegame.Domain
 {
-    public record Vector(int X, int Y);
+    public record Vector(int X, int Y)
+    {
+        public Vector FromChar(char input)
+        {
+            switch (input)
+            {
+                case 'W':
+                case 'w':
+                    {
+                        return new Vector(0, -1);
+                    }
+                case 'S':
+                case 's':
+                    {
+                        return new Vector(0, 1);
+                    }
+                case 'D':
+                case 'd':
+                    {
+                        return new Vector(1, 0);
+                    }
+                case 'A':
+                case 'a':
+                    {
+                        return new Vector(-1, 0);
+                    }
+                default:
+                    {
+                        return new Vector(0, 0);
+                    }
+            }
+        }
+    }
 }
